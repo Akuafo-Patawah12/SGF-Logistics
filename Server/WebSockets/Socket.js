a4465d5e1e0dc78c3a4108810b241df604bf51d8
Akuafo-Patawah12 <burxells873@gmail.com>
2024-10-11 03:40:37 +0000

    new

--- Server/WebSockets/Socket.js
+++ Server/WebSockets/Socket.js
@@ -10,7 +10,7 @@ function initializeSocket(server){
     const io = socketIo(server, {   //Creating connect between server and User Interface  "Realtime WebApp"
       transports: ['websocket'],
         cors: {
-          origin:["https://sgf-logistics.vercel.app/"],
+          origin:["https://sgf-logistics.vercel.app"],
           methods:["POST,GET,PUT,DELETE"],
           allowedHeaders: ['Content-Type'],
           credentials: true
--- Server/index.js
+++ Server/index.js
@@ -18,7 +18,7 @@ const router = require("./AuthRoutes/Router")
 
 
 app.use(cors({
-    origin:["https://sgf-logistics.vercel.app/"],
+    origin:["https://sgf-logistics.vercel.app"],
     credentials: true,
     methods:["POST,GET,PUT,DELETE"], 
     allowedHeaders: ['Content-Type']
