339b8290eeceea50770c84d1ee8b2683f55ec03e
Akuafo-Patawah12 <burxells873@gmail.com>
2024-10-11 03:33:15 +0000

    api update

--- Server/WebSockets/Socket.js
+++ Server/WebSockets/Socket.js
@@ -9,7 +9,7 @@ const AdminPath = require("./AdminNamespace");
 function initializeSocket(server){   
     const io = socketIo(server, {   //Creating connect between server and User Interface  "Realtime WebApp"
         cors: {
-          origin:["http://localhost:3000"],
+          origin:["https://sgf-logistics.vercel.app/"],
           methods:["POST,GET,PUT,DELETE"],
           allowedHeaders: ['Content-Type'],
           credentials: true
