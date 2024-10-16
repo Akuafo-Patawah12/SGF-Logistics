2bbd3a81eef0c2e3b43bd9b54bc7cecdc2303e6e
Akuafo-Patawah12 <burxells873@gmail.com>
2024-10-16 23:40:09 +0000

    update new

--- Client/src/Pages/Components/FAQsAccordion.jsx
+++ Client/src/Pages/Components/FAQsAccordion.jsx
@@ -6,9 +6,9 @@ const FAQsAccordion = (props) => {
     const[accordion,setAccordion] = useState(false)
   return (
     
-        <div style={{background:"#eee",borderRadius:"5px", width:"95%",border:"1px solid #ddd",borderBottom:"5px solid #eee", marginInline:"auto", height:`${accordion?"auto":"50px"}`,overflow:'hidden', animation:`${accordion?"animate 0.3s ease-in":"animateDown 0.3s ease-out"}`}}>
+        <div style={{background:"#eee",borderRadius:"5px", width:"95%",border:"1px solid #ddd",borderBottom:"5px solid #eee", marginInline:"auto", height:`${accordion?"auto":"60px"}`,overflow:'hidden', animation:`${accordion?"animate 0.3s ease-in":"animateDown 0.3s ease-out"}`}}>
             <div style={{display:"flex",justifyContent:"space-between",alignItems:"center",width:"100%",marginBottom:"5px"}}>
-            <p style={{color:"#222",display:"flex",height:"50px",maxWidth:"70%",alignItems:"center ", width:"auto",marginLeft:"8px",fontSize:"16px",fontWeight:"600"}}>{props.Questions}</p>
+            <p style={{color:"#222",display:"flex",height:"60px",maxWidth:"70%",alignItems:"center ", width:"auto",marginLeft:"8px",fontSize:"16px",fontWeight:"600"}}>{props.Questions}</p>
                 <button className="accordion_btn" style={{width:"30px",height:"30px",marginRight:"8px",border:"1px solid #aaa",borderRadius:"50%",rotate:`${accordion? "":"180deg"}`, transition:"all 0.3s"}} onClick={()=> setAccordion(prev=> !prev)}>
                    <UpOutlined /> 
                 </button>
