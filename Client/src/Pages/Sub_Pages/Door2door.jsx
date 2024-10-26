4e0889f5c99400e48678b0cf4bd9233dd7223ff6
Akuafo-Patawah12 <burxells873@gmail.com>
2024-10-26 23:35:34 +0000

    commit and push

--- Client/src/Pages/Sub_Pages/Door2door.jsx
+++ Client/src/Pages/Sub_Pages/Door2door.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { DollarCircleOutlined, ContainerOutlined, ShopOutlined, SmileOutlined } from '@ant-design/icons';
+import "./SubPages.css"
 
 const Door2door = () => {
   return (
@@ -13,11 +14,15 @@ const Door2door = () => {
         </div>
         
         <section className="freight">
+    <div>
       <h2>Door to door delivery</h2>
         <p>
             Our Door-to-Door Delivery Service offers a hassle-free solution, managing the entire logistics 
             process from pickup to final delivery. 
         </p>
+        </div>
+
+        <div>
       <h3>Key Benefits:</h3>
       <ul>
         <li><strong>Hassle-Free: </strong> We handle every aspect of the delivery, from our warehouse to your 
@@ -25,12 +30,17 @@ const Door2door = () => {
         <li><strong>Secure Handling:</strong> Your goods are managed with the utmost care at every stage of the 
         journey.</li>
       </ul>
+      </div>
+
+      <div>
       <h3>How it Works:</h3>
       <p>
          Once your shipment is ready, we take care of everything—customs clearance, transportation, and 
         delivery directly to the recipient’s location, making the process easy and efficient.
       </p>
+      </div>
 
+      <div>
       <h3>Ideal For:</h3>
       <ul>
         <li><ShopOutlined style={{ color: '#004d80', marginRight: '8px' }} />
@@ -40,6 +50,7 @@ const Door2door = () => {
           <strong>Clients looking for a stress-free shipping experience</strong>
         </li>
       </ul>
+      </div>
     </section>
     </div>
   )
