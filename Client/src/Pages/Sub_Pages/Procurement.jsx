d29d66eeeb7d045854c6741b00dc4550c913b1cc
Akuafo-Patawah12 <burxells873@gmail.com>
2024-11-16 15:42:43 +0000

    update

--- Client/src/Pages/Sub_Pages/Procurement.jsx
+++ Client/src/Pages/Sub_Pages/Procurement.jsx
@@ -8,7 +8,7 @@ const Procurement = () => {
     <div className="cont">
       <div className='Freight_image_header'>
            <section style={{position:"absolute",inset:"0",background:"rgb(0,0,0,0.3)",display:"flex",alignItems:"center",justifyContent:"center",color:"white"}}>
-               <p style={{fontSize:"40px",fontWeight:"700",textAlign:"center",width:"",marginInline:"auto"}}>Free Procurement & Sourcing Training .</p>
+               <p style={{fontSize:"40px",fontWeight:"700",textAlign:"center",maxWidth:"80%",marginInline:"auto"}}>Free Procurement & Sourcing Training .</p>
            </section>
 
            
