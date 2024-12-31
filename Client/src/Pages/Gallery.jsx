2d7db74efd97d8b91141f181e9e4b394dbdb4173
Akuafo-Patawah12 <burxells873@gmail.com>
2024-12-31 19:23:40 +0000

    update

--- Client/src/Pages/Gallery.jsx
+++ Client/src/Pages/Gallery.jsx
@@ -3,11 +3,11 @@ import "./Gallery.css"
 const Gallery = () => {
   return (
     <div class="grid-container">
-    <div class="grid-item item-1">1</div>
-    <div class="grid-item item-2">2</div>
-    <div class="grid-item item-3">3</div>
-    <div class="grid-item item-4">4</div>
-    <div class="grid-item item-5">5</div>
+    <div class="grid-item item-1"><img src="../SFG_images/Grid_1.jpg" alt="Grid_1"></img></div>
+    <div class="grid-item item-2"><img src="../SFG_images/Grid_2.jpg" alt="Grid_2"></img></div>
+    <div class="grid-item item-3"><img src="../SFG_images/Grid_3.jpg" alt="Grid_3"></img></div>
+    <div class="grid-item item-4"><img src="../SFG_images/Grid_4.jpg" alt="Grid_4"></img></div>
+    <div class="grid-item item-5"><img src="../SFG_images/Grid_5.jpg" alt="Grid_5"></img></div>
   </div>
   )
 }
