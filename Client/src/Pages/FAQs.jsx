3ad2bfe82752a420148d3eb69c2830b61e965bbe
Akuafo-Patawah12 <burxells873@gmail.com>
2025-02-02 01:24:38 +0000

    update

--- Client/src/Pages/FAQs.jsx
+++ Client/src/Pages/FAQs.jsx
@@ -57,7 +57,7 @@ const FAQs = () => {
                 </span>
               </button>
               {activeIndex === index && (
-                <div className="faq-answer">{faq.answer}</div>
+                <div className="faq-answer" >{faq.answer}</div>
               )}
             </div>
           ))}
