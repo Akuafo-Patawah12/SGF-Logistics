1a1856aae11d15331fe41690dacb85e7b014992f
Akuafo-Patawah12 <burxells873@gmail.com>
2025-02-27 02:59:25 +0000

    update socket and index file

--- Server/index.js
+++ Server/index.js
@@ -18,7 +18,7 @@ const router = require("./Router/Router")
 
 
 app.use(cors({
-    origin:["http://localhost:3000"],
+    origin:["https://sfghanalogistics.com"],
     credentials: true,
     methods:["POST,GET,PUT,DELETE"], 
     allowedHeaders: ["Content-Type"] // Common headers
