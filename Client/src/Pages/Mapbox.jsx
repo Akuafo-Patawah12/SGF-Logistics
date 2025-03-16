8d9278129fde2af3f7decb7e42a927c5304d0448
Akuafo-Patawah12 <burxells873@gmail.com>
2025-03-16 17:27:48 +0000

    update

--- Client/src/Pages/Mapbox.jsx
+++ Client/src/Pages/Mapbox.jsx
@@ -344,14 +344,31 @@ useEffect(() => {
 
       {/* Add Line */}
       <Source id="line-source" type="geojson" data={lineGeoJSON}>
+      <Layer
+    id="route-border"
+    type="line"
+    paint={{
+      "line-color": "#333", // Border color
+      "line-width": 1, // Slightly thicker than main line
+    }}
+  />
         <Layer
           id="line-layer"
           type="line"
           paint={{
-            "line-color": "#8A2BE2", // BlueViolet line color
-            "line-width": 3, // Line thickness
+            "line-color": "#eee", // BlueViolet line color
+            "line-width": 2, // Line thickness
           }}
         />
+
+<Layer
+    id="route-border"
+    type="line"
+    paint={{
+      "line-color": "#333", // Border color
+      "line-width": 1, // Slightly thicker than main line
+    }}
+  />
       </Source>
 
      
