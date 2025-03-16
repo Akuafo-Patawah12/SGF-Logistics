c00e432b23a2da5f34c7da7693453e8e6502b7ad
Akuafo-Patawah12 <burxells873@gmail.com>
2025-03-16 18:03:02 +0000

    update configurations

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,16 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
+import visualizer from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), 
+  plugins: [react(),
+    visualizer({
+      filename: "stats.html", // Output file for visualization
+      open: true, // Automatically open the report in the browser
+      gzipSize: true, // Show gzip size
+      brotliSize: true, // Show brotli size
+    }), 
     svgr({
       svgrOptions: {
         icon: true, // Optional: Treat SVGs as icons
       },
     }),
   ],
-
+  
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendors': ['react', 'react-dom'], // Example chunk splitting
+          'mapbox': ['mapbox-gl']
+        }
+      }
+    }
+  }
  
 })
