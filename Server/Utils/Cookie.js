367cee69d66e386eb151e2a36cedb3e8d47e89c4
Akuafo-Patawah12 <burxells873@gmail.com>
2025-03-17 11:00:09 +0000

    Update Cookie.js

--- Server/Utils/Cookie.js
+++ Server/Utils/Cookie.js
@@ -11,7 +11,7 @@ function sendCookie(payload,rememberMe,res){
 
     res.cookie('refreshToken', refresh_token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', // Only send over HTTPS
+        secure: true,
         sameSite: "None", // Required for cross-origin requests
         path: "/",
         maxAge: rememberMe ? 30 * 24 * 60 * 60 * 1000 : 60 * 60 * 1000 // 30 days or 1 hour
