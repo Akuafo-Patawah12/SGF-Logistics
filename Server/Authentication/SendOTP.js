b96f8c9263f6662de14276089109805895b02edd
Akuafo-Patawah12 <burxells873@gmail.com>
2025-03-18 12:33:21 +0000

    update

--- Server/Authentication/SendOTP.js
+++ Server/Authentication/SendOTP.js
@@ -6,7 +6,23 @@ require("dotenv").config()
 async function sendVerificationEmail(email, code) {
 try{
     console.log(email,code)
-    let transporter= transport()
+    
+      let transporter =nodemailer.createTransport({  //create transport allows to create communicating channel
+                      host: 'smtp.gmail.com',
+                      port: 465,
+                      secure: true,
+                      auth: {
+                          user: process.env.EMAIL,//email that will be sending messages from the server to the client
+                          pass: process.env.PASSWORD  //generated password form less secured apps from Google
+                      },
+                      tls: {
+                          rejectUnauthorized: false, //do not reject self-signed certificates  
+                        },
+                      })
+  
+                     
+  
+  
   
     let mailOptions = {
       from: `"SF Ghana Logistics" <${process.env.EMAIL}>`,
